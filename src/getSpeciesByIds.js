91b901cdf0f6768527a93b24b4518e8207ceeab5
jaidernunes <jaidernf@gmail.com>
2022-08-26 16:57:30 -0300

    req 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqu
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  return data.species.filter((value) => ids.includes(value.id));
 }
+// conceito ficou claro e consegui resolver meu erro na mentoria com a inigualável Nat
 
 module.exports = getSpeciesByIds;
