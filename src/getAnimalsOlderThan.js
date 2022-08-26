93017dafc43a38ddc6bb192b42b9b4f589c12745
jaidernunes <jaidernf@gmail.com>
2022-08-26 18:40:54 -0300

    req 3 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+ data.species.name.every((value) => data.species.residents['age']);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(anyName) {
   // seu código aqui
+  if (anyName === undefined) {
+    return {};
+  }
+  return employees.find((name) => name.firstName === anyName || name.lastName === anyName);
 }
 
 module.exports = getEmployeeByName;
