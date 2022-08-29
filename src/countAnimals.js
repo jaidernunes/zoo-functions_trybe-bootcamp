bee21e30438d798e1a6af3810106ecfe874519c7
jaidernunes <jaidernf@gmail.com>
2022-08-29 17:54:33 -0300

    req 5 halfway

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const filterName = data.species.map((specie) => `${specie.name}: ${specie.residents.length} `);
+  return filterName;
+  // USE SORT
 }
-
+console.log(countAnimals());
 module.exports = countAnimals;
