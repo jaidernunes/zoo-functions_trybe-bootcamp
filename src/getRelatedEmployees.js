0bf743c811978fd325b1ef576f272849d4bccd5e
jaidernunes <jaidernf@gmail.com>
2022-08-29 16:46:35 -0300

    req 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,11 +2,18 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  //CHECK IF ID == SOME > RESIDENTS.MANGAGERS
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const returnedArr = [];
+  const managed = data.employees.filter((employee) => employee.managers.includes(managerId));
+  managed.forEach((el) => returnedArr.push(`${el.firstName} ${el.lastName}`));
+  if (returnedArr.length <= 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return returnedArr;
 }
 
 module.exports = { isManager, getRelatedEmployees };
