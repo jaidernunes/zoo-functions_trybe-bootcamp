404462a01f147f3f35b286daf1156ce492d249f8
jaidernunes <jaidernf@gmail.com>
2022-08-30 16:11:12 -0300

    req 8.1 ok (reduce)

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,7 +2,29 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  return entrants.reduce((acc, entrant) => {
+    const obj = acc;
+    if (entrant.age < 18) {
+      obj.child += 1;
+    }
+    if (entrant.age >= 50) {
+      obj.senior += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      obj.adult += 1;
+    }
+    return obj;
+  },
+  { child: 0, adult: 0, senior: 0 });
 }
+// const xEntrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+// console.log(countEntrants(xEntrants));
 
 function calculateEntry(entrants) {
   // seu código aqui
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,11 +3,11 @@ const data = require('../data/zoo_data');
 function countAnimals(animal) {
   // seu código aqui
   if (typeof animal !== 'object') {
-    const obj1 = {};
+    const obj = {};
     data.species.forEach((specie) => {
-      obj1[specie.name] = specie.residents.length;
+      obj[specie.name] = specie.residents.length;
     });
-    return obj1;
+    return obj;
   }
   if (Object.keys(animal).length === 2) {
     const filterName = data.species.find((specie) => specie.name === animal.specie);
@@ -19,28 +19,5 @@ function countAnimals(animal) {
     return filterName.residents.length;
   }
 }
-
-console.log(countAnimals({ specie: 'bears', sex: 'female' }));
-console.log(Object.keys({ specie: 'bears', sex: 'female' }).length);
-// const obj1 = {};
-// data.species.forEach((specie) => {
-//   obj1[specie.name] = specie.residents.length;
-// });
-// return obj1;
-// const returnedObj = {};
-// const animalName = data.species.name;
-// const resident = data.species.residents;
-// const filter1 = data.species.forEach(specie) => {
-//   specie.name
-// }
-// return filterName;
-// const filterName = data.species.map((specie) => `${specie.name}: ${specie.residents.length} `);
-// return Object.assign(returnedObj, filterName);
-// USE SORT
-// data.species.reduce((obj, specie) => {
-//   const returnedObj = obj;
-//   returnedObj[specie.name] = specie.residents.length;
-//   return returnedObj;
-// }, {});
 
 module.exports = countAnimals;
