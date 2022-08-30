7873ca0fc4e72fe7842b95dbd9b9c6a0dcbf196d
jaidernunes <jaidernf@gmail.com>
2022-08-30 09:25:46 -0300

    req 5 - 1 pass

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,9 +2,29 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-  const filterName = data.species.map((specie) => `${specie.name}: ${specie.residents.length} `);
-  return filterName;
-  // USE SORT
+  const obj1 = {};
+  data.species.forEach((specie) => {
+    obj1[specie.name] = specie.residents.length;
+  });
+  return obj1;
 }
+
 console.log(countAnimals());
+
+// const returnedObj = {};
+// const animalName = data.species.name;
+// const resident = data.species.residents;
+// const filter1 = data.species.forEach(specie) => {
+//   specie.name
+// }
+// return filterName;
+// const filterName = data.species.map((specie) => `${specie.name}: ${specie.residents.length} `);
+// return Object.assign(returnedObj, filterName);
+// USE SORT
+// data.species.reduce((obj, specie) => {
+//   const returnedObj = obj;
+//   returnedObj[specie.name] = specie.residents.length;
+//   return returnedObj;
+// }, {});
+
 module.exports = countAnimals;
