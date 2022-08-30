a1cb467f1e5b6624775a89ce48a9d714f3686622
jaidernunes <jaidernf@gmail.com>
2022-08-30 19:40:09 -0300

    req 9.2 halfway

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -17,15 +17,6 @@ function countEntrants(entrants) {
   },
   { child: 0, adult: 0, senior: 0 });
 }
-// const xEntrants = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-// console.log(countEntrants(xEntrants));
 
 function calculateEntry(entrants) {
   // seu código aqui
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,6 +2,25 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const office = (day) => `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+  const exhibit = (day) => {
+    const arr = data.species.filter((specie) => specie.availability.includes(day));
+    return arr.map((specie) => specie.name);
+  };
+
+  const week = {
+    Tuesday: { officeHour: office('Tuesday'), exhibition: exhibit('Tuesday') },
+    Wednesday: { officeHour: office('Wednesday'), exhibition: exhibit('Wednesday') },
+    Thursday: { officeHour: office('Thursday'), exhibition: exhibit('Thursday') },
+    Friday: { officeHour: office('Friday'), exhibition: exhibit('Friday') },
+    Saturday: { officeHour: office('Saturday'), exhibition: exhibit('Saturday') },
+    Sunday: { officeHour: office('Sunday'), exhibition: exhibit('Sunday') },
+    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+  };
+  return week;
 }
+getSchedule();
 
 module.exports = getSchedule;
+
+
