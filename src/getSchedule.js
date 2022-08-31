de4431c52eccf383ff9e4fa48134220b3aa3708d
jaidernunes <jaidernf@gmail.com>
2022-08-31 15:33:41 -0300

    req 9 done!

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,26 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-  const office = (day) => `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
-  const exhibit = (day) => {
-    const arr = data.species.filter((specie) => specie.availability.includes(day));
-    return arr.map((specie) => specie.name);
-  };
+const office = (day) => `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+const exhibit = (day) => {
+  const arr = data.species.filter((specie) => specie.availability.includes(day));
+  return arr.map((specie) => specie.name);
+};
+const week = {
+  Tuesday: { officeHour: office('Tuesday'), exhibition: exhibit('Tuesday') },
+  Wednesday: { officeHour: office('Wednesday'), exhibition: exhibit('Wednesday') },
+  Thursday: { officeHour: office('Thursday'), exhibition: exhibit('Thursday') },
+  Friday: { officeHour: office('Friday'), exhibition: exhibit('Friday') },
+  Saturday: { officeHour: office('Saturday'), exhibition: exhibit('Saturday') },
+  Sunday: { officeHour: office('Sunday'), exhibition: exhibit('Sunday') },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+const daysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-  const week = {
-    Tuesday: { officeHour: office('Tuesday'), exhibition: exhibit('Tuesday') },
-    Wednesday: { officeHour: office('Wednesday'), exhibition: exhibit('Wednesday') },
-    Thursday: { officeHour: office('Thursday'), exhibition: exhibit('Thursday') },
-    Friday: { officeHour: office('Friday'), exhibition: exhibit('Friday') },
-    Saturday: { officeHour: office('Saturday'), exhibition: exhibit('Saturday') },
-    Sunday: { officeHour: office('Sunday'), exhibition: exhibit('Sunday') },
-    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-  };
+function getSchedule(scheduleTarget) {
+  if (data.species.some((specie) => specie.name === scheduleTarget)) {
+    return data.species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+
+  if (daysArr.some((day) => day === scheduleTarget)) {
+    const dayObj = { [scheduleTarget]: week[scheduleTarget] };
+    return dayObj;
+  }
+  // if (data.species.some((specie) => specie.availability.includes(scheduleTarget))) {
+  //   return week[scheduleTarget];
+  // }
+
   return week;
 }
-getSchedule();
-
+// getSchedule();
+console.log(getSchedule('Monday'));
 module.exports = getSchedule;
-
-
