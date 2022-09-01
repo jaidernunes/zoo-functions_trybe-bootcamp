265d8ce7d2823fff344f89b8590ca6e6f161a40c
jaidernunes <jaidernf@gmail.com>
2022-09-01 17:07:28 -0300

    req 11 despair

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 // const data = require('../data/zoo_data');
 const { employees, species } = require('../data/zoo_data');
 
@@ -8,32 +9,48 @@ const returnedObj = {
   locations: [],
 };
 
-// const findSpecie = findEmployee.responsibleFor.map((specie) => data.species.id)
-// const findSpecie = data.species.filter((value) => ids.includes(value.id))
+const allObject = {
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+  species: species.filter((specie) => employee.responsibleFor.includes(specie.id)).map((specie) => specie.name),
+  locations: species.filter((specie) => employee.responsibleFor.includes(specie.id)).map((specie) => specie.location),
+};
+
+const allEmployeesArr = [];
 
 function getEmployeesCoverage(employeeRef) {
   // seu código aqui
-  const findEmployee = employees.find((employee) => (employee.id === employeeRef.id
-    || employee.firstName === employeeRef.name
-    || employee.lastName === employeeRef.name));
-  const findSpecie = species.filter((specie) => findEmployee.responsibleFor.includes(specie.id));
-  const speciesNames = findSpecie.map((specie) => specie.name);
-  const speciesLocation = findSpecie.map((specie) => specie.location);
-
-  returnedObj.id = findEmployee.id;
-  returnedObj.fullName = `${findEmployee.firstName} ${findEmployee.lastName}`;
-  returnedObj.species = speciesNames;
-  returnedObj.locations = speciesLocation;
-  return returnedObj;
+  return employees.map((employee) => allObject);
 }
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+// 3 reqs ok !!!!!!! \/
+  // if ((employee) => (employee.id === employeeRef.id
+  //   || employee.firstName === employeeRef.name
+  //   || employee.lastName === employeeRef.name)) {
+  //   const findEmployee = employees.find((employee) => (employee.id === employeeRef.id
+  //       || employee.firstName === employeeRef.name
+  //       || employee.lastName === employeeRef.name));
+  //   const findSpecie = species.filter((specie) => findEmployee.responsibleFor.includes(specie.id));
+  //   const speciesNames = findSpecie.map((specie) => specie.name);
+  //   const speciesLocation = findSpecie.map((specie) => specie.location);
+  // }
+  // returnedObj.id = findEmployee.id;
+  // returnedObj.fullName = `${findEmployee.firstName} ${findEmployee.lastName}`;
+  // returnedObj.species = speciesNames;
+  // returnedObj.locations = speciesLocation;
+  // return returnedObj;
 
-// const expected = {
-//   id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad',
-//   fullName: 'Sharonda Spry',
-//   species: [ 'otters', 'frogs' ],
-//   locations: [ 'SE', 'SW' ],
-// };
-
+  // ALL RETURN - VALORES RETORNAM SEMPRE DO PRIMEIRO FOREACH
+  // const allEmployees = () => employees.forEach((employee) => {
+  //   const findSpecie = species.filter((specie) => employee.responsibleFor.includes(specie.id));
+  //   const speciesNames = findSpecie.map((specie) => specie.name);
+  //   const speciesLocation = findSpecie.map((specie) => specie.location);
+  //   returnedObj.id = employee.id;
+  //   returnedObj.fullName = `${employee.firstName} ${employee.lastName}`;
+  //   returnedObj.species = speciesNames;
+  //   returnedObj.locations = speciesLocation;
+  //   allEmployeesArr.push(returnedObj);
+  // });
+console.log(getEmployeesCoverage());
+// '4b40a139-d4dc-4f09-822d-ec25e819a5ad'
 module.exports = getEmployeesCoverage;
