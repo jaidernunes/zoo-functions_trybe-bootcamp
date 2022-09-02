14fb7c32023743d2e9b05e220889af252ea5ef3c
jaidernunes <jaidernf@gmail.com>
2022-09-02 10:23:18 -0300

    req 11 cleaned

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,6 @@
 // const data = require('../data/zoo_data');
 const { employees, species } = require('../data/zoo_data');
 
-// const returnedObj = {
-//   id: findEmployee.id,
-//   fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
-//   species: speciesNames,
-//   locations: speciesLocation,
-// };
-// const returnedObj = {
-//   id: 'id',
-//   fullName: 'name last',
-//   species: [],
-//   locations: [],
-// };
-
 function getEmployeesCoverage(eR) {
   if (eR === undefined) {
     return employees.map((e) => ({ id: e.id,
@@ -36,31 +23,3 @@ function getEmployeesCoverage(eR) {
 }
 
 module.exports = getEmployeesCoverage;
-
-// 3 reqs ok !!!!!!! \/
-// const findEmployee = employees.find((employee) => (employee.id === employeeRef.id
-//   || employee.firstName === employeeRef.name
-//   || employee.lastName === employeeRef.name));
-// const findSpecie = species.filter((specie) => findEmployee.responsibleFor.includes(specie.id));
-// const speciesNames = findSpecie.map((specie) => specie.name);
-// const speciesLocation = findSpecie.map((specie) => specie.location);
-
-// returnedObj.id = findEmployee.id;
-// returnedObj.fullName = `${findEmployee.firstName} ${findEmployee.lastName}`;
-// returnedObj.species = speciesNames;
-// returnedObj.locations = speciesLocation;
-// return returnedObj;
-// }
-
-// ALL RETURN - VALORES RETORNAM SEMPRE DO PRIMEIRO FOREACH
-// const allEmployees = () => employees.map((employee) => {
-//   const findSpecie = species.filter((specie) => employee.responsibleFor.includes(specie.id));
-//   const speciesNames = findSpecie.map((specie) => specie.name);
-//   const speciesLocation = findSpecie.map((specie) => specie.location);
-//   ({
-//   id: employee.id,
-//   fullName:`${employee.firstName} ${employee.lastName}`,
-//   species:speciesNames,
-//   locations: speciesLocation,
-//   });
-// })
