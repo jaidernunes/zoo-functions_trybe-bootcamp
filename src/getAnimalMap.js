1438e8202a611da9a6b9ca8a7df761bdc3546420
jaidernunes <jaidernf@gmail.com>
2022-09-02 20:55:00 -0300

    reqs 6, 7, 15 and 16 ok!

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,45 +1,72 @@
 const { species } = require('../data/zoo_data');
 
-// const obj = {
-//   NE: ['lions', 'giraffes'],
-//   NW: ['tigers', 'bears', 'elephants'],
-//   SE: ['penguins', 'otters'],
-//   SW: ['frogs', 'snakes'],
-// };
-
-// const ne = species.filter((specie) => specie.location === 'NE').map((specie) => specie.name);
-// const nw = species.filter((specie) => specie.location === 'NW').map((specie) => specie.name);
-// const se = species.filter((specie) => specie.location === 'SE').map((specie) => specie.name);
-// const sw = species.filter((specie) => specie.location === 'SW').map((specie) => specie.name);
-
 const ne = species.filter((specie) => specie.location === 'NE');
 const nw = species.filter((specie) => specie.location === 'NW');
 const se = species.filter((specie) => specie.location === 'SE');
 const sw = species.filter((specie) => specie.location === 'SW');
 
-// const noParamObj = {
-//   NE: ne.map((specie) => specie.name),
-//   NW: nw.map((specie) => specie.name),
-//   SE: se.map((specie) => specie.name),
-//   SW: sw.map((specie) => specie.name),
-// };
+const noParamObj = {
+  NE: ne.map((specie) => specie.name),
+  NW: nw.map((specie) => specie.name),
+  SE: se.map((specie) => specie.name),
+  SW: sw.map((specie) => specie.name),
+};
+
+const namesNE = ne.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) }));
+const namesNw = nw.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) }));
+const namesSE = se.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) }));
+const namesSW = sw.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) }));
+
+const includeNamesObj = {
+  NE: namesNE,
+  NW: namesNw,
+  SE: namesSE,
+  SW: namesSW,
+};
+
+const namesNEsort = ne.map((s) => ({ [s.name]: s.residents.map((sp) => sp.name).sort() }));
+const namesNwsort = nw.map((s) => ({ [s.name]: s.residents.map((sp) => sp.name).sort() }));
+const namesSEsort = se.map((s) => ({ [s.name]: s.residents.map((sp) => sp.name).sort() }));
+const namesSWsort = sw.map((s) => ({ [s.name]: s.residents.map((sp) => sp.name).sort() }));
+
+const includeNamesObjSorted = {
+  NE: namesNEsort,
+  NW: namesNwsort,
+  SE: namesSEsort,
+  SW: namesSWsort,
+};
+
+const maleSort = {
+  NE: ne.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'male').map((sp) => sp.name) })),
+  NW: nw.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'male').map((sp) => sp.name) })),
+  SE: se.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'male').map((sp) => sp.name) })),
+  SW: sw.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'male').map((sp) => sp.name) })),
+};
+const femaleSort = {
+  NE: ne.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'female').map((sp) => sp.name) })),
+  NW: nw.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'female').map((sp) => sp.name) })),
+  SE: se.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'female').map((sp) => sp.name) })),
+  SW: sw.map((s) => ({ [s.name]: s.residents.filter((x) => x.sex === 'female').map((sp) => sp.name) })),
+};
 
 function getAnimalMap(options) {
   // seu código aqui
-  // if (options.includeNames) {
-  if (options.includeNames === true) {
-    return {
-      NE: ne.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) })),
-      NW: nw.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) })),
-      SE: se.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) })),
-      SW: sw.map((specie) => ({ [specie.name]: specie.residents.map((sp) => sp.name) })),
-    };
-  }
+  return femaleSort;
+  // if (options && options.includeNames) {
+  //   if (options.sorted === true) {
+  //     if (options.sex === 'male') {
+  //       return maleSort;
+  //     };
+  //     if (options.sex === 'female') {
+  //       return femaleSort;
+  //     };
+  //     }
+  //     return includeNamesObjSorted;
+  //   }
+  //   return includeNamesObj;
+  // return noParamObj;
 }
-// ALL RETURN
-// return noParamObj;
-// }
 
-// console.log(getAnimalMap({ includeNames: true }));
+// console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,58 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('verifica se ao não receber nenhum parâmetro a função retorna o objeto esperado', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 } });
+  });
+
+  it('verifica se ao receber os param \'Monday\', \'09:00-AM\') retorna \'The zoo is closed\'', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+
+  it('verifica se ao receber os param \'Tuesday\', \'09:00-AM\') retorna \'The zoo is open\'', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+
+  it('verifica se recebe erro ao colocar uma segunda string inválida', () => {
+    expect(() => (getOpeningHours('Monday', 'a9-00-AM'))).toThrow('The hour should represent a number');
+  });
+
+  it('verifica se recebe erro ao não receber AM ou PM no segundo parâmetro', () => {
+    expect(() => (getOpeningHours('Tuesday', '09:00-ZZ'))).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+
+  it('verifica se recebe erro ao receber horas inválidas', () => {
+    expect(() => (getOpeningHours('Tuesday', '40:00-AM'))).toThrow('The hour must be between 0 and 12');
+  });
+
+  it('verifica se recebe erro ao receber minutos inválidos', () => {
+    expect(() => (getOpeningHours('Tuesday', '10:61-AM'))).toThrow('The minutes must be between 0 and 59');
+  });
+
+  it('verifica se recebe erro ao receber um dia inválido', () => {
+    expect(() => (getOpeningHours('Noday', '09:00-AM'))).toThrow('The day must be valid. Example: Monday');
+  });
+
+  // it('', () => {
+  //   expect().toEqual();
+  // });
+
+  // it('', () => {
+  //   expect().toEqual();
+  // });
+
+  // it('', () => {
+  //   expect().toEqual();
+  // });
+
+  // it('', () => {
+  //   expect().toEqual();
+  // });
+});
